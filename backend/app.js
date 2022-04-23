3dd41d3a51af5c8bccd6a5a9f49dcab3b72ac0a7
Arbuznik <arbyzov.nikita@yandex.ru>
2022-04-23 21:36:17 +0300

    change port to 3001

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { handleErrors } = require('./middlewares/errors')
 const { requestLogger, errorLogger } = require('./middlewares/logger')
 const { NotFoundError } = require('./middlewares/errors/NotFoundError')
 
-const { PORT = 3000 } = process.env
+const { PORT = 3001 } = process.env
 const app = express()
 
 app.use(bodyParser.json())
